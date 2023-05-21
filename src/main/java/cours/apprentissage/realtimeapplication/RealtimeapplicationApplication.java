92708b7d600d0f7dab0128aadc8fb5f462419d00
Abdoul Karim DIALLO <abdoulkarimfoly1996@gmail.com>
2023-05-21 23:48:17 +0000

    first commit websocket whtie spring boot

---
+++ src/main/java/cours/apprentissage/realtimeapplication/RealtimeapplicationApplication.java
@@ -0,0 +1,13 @@
+package cours.apprentissage.realtimeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cours/apprentissage/realtimeapplication/RealtimeapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package cours.apprentissage.realtimeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
